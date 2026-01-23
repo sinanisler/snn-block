8ca9784e4ff01354aed47435653872c618f7632e
sinanisler <sinan@sinanisler.com>
2026-01-23 13:20:25 +0300

    .

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ require_once SNN_PATH . 'includes/ai/abilities/list-taxonomies.php';
 require_once SNN_PATH . 'includes/block-editor-settings.php';
 require_once SNN_PATH . 'includes/media-image-opt.php';
 require_once SNN_PATH . 'includes/wp-admin-dashboard-widgets.php';
-
+require_once SNN_PATH . 'includes/auto-update-snn-block.php';
 
 
 
